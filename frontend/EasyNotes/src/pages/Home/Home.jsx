3269c288631b8baf3d503544cb0f044cfeb03066
Raj Kumar <rajkumarsolanki907@gmail.com>
2024-09-10 02:36:59 +0530

    NavBar and SearchBar Logic Update also Logout Feature

--- frontend/EasyNotes/src/pages/Home/Home.jsx
+++ frontend/EasyNotes/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+    <Navbar />
+    
+    </>
   )
 }
 
