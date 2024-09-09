827b1120de4848667eb95960afd45bfe268e883f
Raj Kumar <rajkumarsolanki907@gmail.com>
2024-09-10 02:45:45 +0530

    user-name

--- frontend/EasyNotes/src/components/Cards/ProfileInfo.jsx
+++ frontend/EasyNotes/src/components/Cards/ProfileInfo.jsx
@@ -5,11 +5,11 @@ const ProfileInfo = ( { onLogout } ) => {
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-        {getInitials("John Willam")}
+        {getInitials("New User")}
       </div>
 
         <div>
-            <p className="text-sm font-medium">John Willam</p>
+            <p className="text-sm font-medium">New User</p>
             <button className="text-sm text-slate-700 underline" onClick={onLogout}>
                 Logout
             </button>
