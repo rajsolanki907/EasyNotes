98d4ca29cce522423007692101e214ee078072ac
Raj Kumar <rajkumarsolanki907@gmail.com>
2024-09-10 03:59:18 +0530

    Note Card Designed And Implemeted with parameters

--- frontend/EasyNotes/src/pages/Home/Home.jsx
+++ frontend/EasyNotes/src/pages/Home/Home.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Notecard from '../../components/Cards/Notecard'
 
 const Home = () => {
   return (
     <>
     <Navbar />
-    
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4 mt-8">
+      <Notecard 
+      title="Meeting on Monday Morning" 
+      date={"10 August 2024"} 
+      content = "Meeting on Monday 11 august"
+      tags="#Meeting"
+      isPinned={true}
+      onEdit={()=>{}}
+      onDelete={()=>{}}
+      onPinNote={()=>{}}
+      />
+      <Notecard 
+      title="Meeting on Monday Morning" 
+      date={"10 August 2024"} 
+      content = "Meeting on Monday 11 august"
+      tags="#Meeting"
+      isPinned={true}
+      onEdit={()=>{}}
+      onDelete={()=>{}}
+      onPinNote={()=>{}}
+      />
+      <Notecard 
+      title="Meeting on Monday Morning" 
+      date={"10 August 2024"} 
+      content = "Meeting on Monday 11 august"
+      tags="#Meeting"
+      isPinned={true}
+      onEdit={()=>{}}
+      onDelete={()=>{}}
+      onPinNote={()=>{}}
+      />
+      </div>
+    </div>
     </>
   )
 }
