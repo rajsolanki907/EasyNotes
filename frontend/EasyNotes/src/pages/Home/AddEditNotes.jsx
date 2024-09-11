6bcf386bfdf6c929c933b0a7a4e14d6065ed344d
Raj Kumar <rajkumarsolanki907@gmail.com>
2024-09-11 18:26:34 +0530

    Added Tags in the NoteCard

--- frontend/EasyNotes/src/pages/Home/AddEditNotes.jsx
+++ frontend/EasyNotes/src/pages/Home/AddEditNotes.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TagInput from '../../components/Input/TagInput'
 
 const AddEditNotes = () => {
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]); 
   return (
     <div>
       <div className="flex flex-col gap-2">
@@ -9,6 +14,8 @@ const AddEditNotes = () => {
             type="text"
             className="text-2xl text-slate-950 outline-none"
             placeholder="Go To Gym At 5"
+            value = {title}
+            onChange = {({ target }) => setTitle(target.value)}
         />
       </div>
 
@@ -19,11 +26,14 @@ const AddEditNotes = () => {
             className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
             placeholder="Content"
             rows={10}
+            value={content}
+            onChange = {({ target }) => setContent(target.value)}
         />
       </div>
 
       <div className="mt-5">
         <label className="input-label">TAGS</label>
+        <TagInput tags={tags} setTags={setTags}/>
       </div>
       <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
         ADD
