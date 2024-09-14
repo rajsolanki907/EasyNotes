5e7a1b093fed01bbac5da71bbffa79cf67288977
Raj Kumar <rajkumarsolanki907@gmail.com>
2024-09-15 02:39:35 +0530

    Edit Notes Route Backend

--- backend/index.js
+++ backend/index.js
@@ -108,7 +108,7 @@ app.post("/login", async (req, res) => {
     }
 });
 
-// Add Notes request Logic
+// Add Notes request Logic 
 app.post("/add-note", authenticateToken , async (req, res) => {
     const {title, content, tags } =req.body;
     const { user } =req.user;
@@ -144,6 +144,45 @@ app.post("/add-note", authenticateToken , async (req, res) => {
             message: "Internal Server Error",
         });
     }
+});
+
+//Edit Notes Request Logic
+app.put("/edit-note/:noteId", authenticateToken , async (req, res) => {
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body; 
+    const { user } = req.user;
+
+    if(!title && !content && !tags) {
+        return res.status(400).json({ error: true, message: "No changes Provided" });
+    }
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+        if(!note) {
+            return res.status(404).json({ error: true, message: "Note not found" });
+        }
+
+        if(title) note.title=title;
+        if(content) note.content=content;
+        if(tags) note.tags=tags;
+        if(isPinned !== undefined) note.isPinned=isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note edited successfully",
+        });
+    } catch (error) {
+        console.error('Error editing note', error);
+        return res.status(500).json({
+            error: true,
+            message: "Internal Server Error",
+        });
+    }
+
 });
 
 app.listen(8000);
