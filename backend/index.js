7e150f8ef737fe58b98a81b28bfcc89189b58ff3
Raj Kumar <rajkumarsolanki907@gmail.com>
2024-09-22 23:51:46 +0530

    "/ route for backend request signup

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,46 @@ app.use(
     })
 );
 
-app.get("/", (req, res) =>{
-    res.json({data:"hello" });
+app.post("/", async(req, res) =>{
+    const { fullName, email, password } =req.body;
+
+    if(!fullName) {
+        return res.status(400).json({ error: true, message: "Full Name is required" });
+    }
+
+    if(!email) {
+        return res.status(400).json({ error: true, message: "Email is required" });
+    }
+    if(!password)  {
+        return res.status(400).json({ error: true, message: "Password is required" });
+    }
+    const isUser = await User.findOne({email: email});
+
+    if(isUser) {
+        return res.json({
+            error: true,
+            message: "User already exists",
+        });
+    }
+
+    const user= new User({
+        fullName,
+        email,
+        password,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign({ user}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "36000m",
+    });
+
+    return res.json({
+        error: false, 
+        user,
+        accessToken,
+        message: "Registration Successful"
+    });
 });
 
 // Create Account or Signup
